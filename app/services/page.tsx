a482f7e6277ba83c8d3b7c8a11b1557cf263c1b6
ab-atiq <abulbasharatiqur@gmail.com>
2026-08-12 22:17:24 +0600

    service

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,18 +1,10 @@
-export default async function HomePa() {
+export default async function ServicesPage() {
   const response = await fetch(
     "https://ph-l2-a4-fix-it-now-backend-project-drab.vercel.app/api/services",
   );
   const servicesResponse = await response.json();
   const services = Array.isArray(servicesResponse?.data)
     ? servicesResponse.data
-    : [];
-
-  const categoryResponse = await fetch(
-    "https://ph-l2-a4-fix-it-now-backend-project-drab.vercel.app/api/categories",
-  );
-  const categoriesResponse = await categoryResponse.json();
-  const categories = Array.isArray(categoriesResponse?.data)
-    ? categoriesResponse.data
     : [];
 
   return (
@@ -23,14 +15,6 @@ export default async function HomePa() {
             <h2>Name: {service.serviceName}</h2>
             <p>Description: {service.description}</p>
             <p>Price: ${service.basePrice ?? service.baePrice ?? "N/A"}</p>
-          </div>
-        ))}
-      </div>
-      <div>
-        {categories.map((category: any) => (
-          <div key={category.id}>
-            <h2>Category: {category.categoryName}</h2>
-            <p>Description: {category.description}</p>
           </div>
         ))}
       </div>
